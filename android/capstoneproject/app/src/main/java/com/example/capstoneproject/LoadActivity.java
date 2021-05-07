4c1258554968638ba6b1097747553470fd3307ac
Jiwon-Lee <won64312000@naver.com>
2021-05-07 15:42:19 +0900

    로딩화면 추가 및 bottom_Navi 수정중

--- android/capstoneproject/app/src/main/java/com/example/capstoneproject/LoadActivity.java
+++ android/capstoneproject/app/src/main/java/com/example/capstoneproject/LoadActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoadActivity extends Activity {
@@ -12,9 +13,24 @@ public class LoadActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView) ;
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView2) ;
+        imageView1.setImageResource(R.drawable.first_icon);
+        imageView2.setImageResource(R.drawable.brandname);
+
+        Handler handler = new Handler();
+     handler.postDelayed(new Runnable() {
+         @Override
+         public void run() {
+             Intent intent = new Intent(LoadActivity.this, MainActivity.class);
+             startActivity(intent);
+             finish();
+         }
+     },3000);
+
+
 
     }
-
 }
 
 
