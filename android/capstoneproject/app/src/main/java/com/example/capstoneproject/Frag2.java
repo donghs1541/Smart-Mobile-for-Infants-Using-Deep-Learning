37af6e5a2adc0f937c5e34b59ac80ccc67d061c0
donghs1541 <63491788+donghs1541@users.noreply.github.com>
2021-05-07 17:04:51 +0900

    webview 기능 적용
    webview 기능 적용

--- android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag2.java
+++ android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag2.java
@@ -4,24 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 public class Frag2 extends Fragment {
 
     private View view;
+    private WebView mWebView; // 웹뷰 선언
+    private WebSettings mWebSettings; //웹뷰세팅
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag2, container, false);
+        mWebView = (WebView)view.findViewById(R.id.webview_login);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("http://www.naver.com");
 
-    view = inflater.inflate(R.layout.frag2,container,false);
-
-
-
-    return view;
+        return view;
 
     }
 }
