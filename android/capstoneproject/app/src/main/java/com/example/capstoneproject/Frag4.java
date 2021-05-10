d761d9933742fda9ebd7945a6aa022e5a831421d
donghs1541 <63491788+donghs1541@users.noreply.github.com>
2021-05-10 17:52:00 +0900

    설정(listview사용) 기능 추가중
    설정(listview사용) 기능 추가중

--- android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag4.java
+++ android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag4.java
@@ -12,13 +12,17 @@ import androidx.fragment.app.Fragment;
 public class Frag4 extends Fragment {
 
     private View view;
+    private String[] LIST_MENU = {"LIST1","LIST2","LIST3"};
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
     view = inflater.inflate(R.layout.frag4,container,false);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
 
+        ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
 
 
     return view;
