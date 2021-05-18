770bf87801c49350fddf3211061340cfffdf6791
donghs1541 <63491788+donghs1541@users.noreply.github.com>
2021-05-18 16:46:59 +0900

    이름, 생일 db 로드
    이름, 생일 db 로드 기능 추가

--- android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag1.java
+++ android/capstoneproject/app/src/main/java/com/example/capstoneproject/Frag1.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.Color;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,6 +12,7 @@ import android.os.Message;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,12 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -33,16 +41,28 @@ import java.util.Date;
 
 public class Frag1 extends Fragment {
 
-    TextView babyage1,babyage2,babyage3,babyage4,current_data,temper_value,humi_value,dust_value,baby_birth,dust_result;
+    TextView babyage1,babyage2,babyage3,babyage4,current_data,temper_value,humi_value,dust_value,baby_birth,dust_result,NameText;
     ImageView dust_image;
     String SenSorValue;
     String str,temp_temper,temp_humi,temp_dust,face_detection_value;
     String babyage1_year,babyage1_mon,babyage2_mon,babyage2_date, babyage3_value,babyage3_value_remain, babyage4_value;
     String dust_contition;
+
     public final String CHANNEL_ID = "my_notification_channel";
     public final int NOTIFICATION_ID = 101;
     private View view;
+
+
+
+    private String date1;
+    private String date2; //아기 탄생일 불러오기 위한 String
+    private Date FirstDate;
+    private Date SecondDate;
+
+
     MyHandler mHandler = new MyHandler();
+
+
 
     public String getSenSorValue(){ //센서값 보내주기
         return SenSorValue;
@@ -121,8 +141,6 @@ public class Frag1 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-
     view = inflater.inflate(R.layout.frag1,container,false);
         babyage1 = (TextView)view.findViewById(R.id.babyage1);
         babyage2 = (TextView)view.findViewById(R.id.babyage2);
@@ -135,61 +153,12 @@ public class Frag1 extends Fragment {
         dust_value = (TextView)view.findViewById(R.id.dust_value);
         dust_image = (ImageView)view.findViewById(R.id.imageView4);
         dust_result = (TextView)view.findViewById(R.id.dust_result);
-
-        //현재 시간 구하기
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        String getTime = sdf.format(date);
+        NameText = (TextView)view.findViewById(R.id.babyname);
 
 
-        //태어난지 얼마나 되었는지 날짜 차이 구하기
-        String date1 = getTime;
-        String date2 = "2020-07-10";
+        final Frag1.JsonParse jsonParse = new Frag1.JsonParse();      // AsyncTask 생성
+        jsonParse.execute("http://113.198.234.49:7776/info_load_birthday.php");     // 이름과 생일을 db에서 불러오는 부분
 
-        try{
-            // date1, date2 두 날짜를 parse()를 통해 Date형으로 변환.
-            Date FirstDate = sdf.parse(date1);
-            Date SecondDate = sdf.parse(date2);
-
-            // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
-            // 연산결과 -950400000. long type 으로 return 된다.
-            long calDate = FirstDate.getTime() - SecondDate.getTime();
-
-            // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
-            // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
-            long calDateDays = calDate / ( 24*60*60*1000);
-            calDateDays = Math.abs(calDateDays);
-
-            //날짜 데이터 처리 부분
-            babyage1_year = Long.toString( calDateDays/30/12);
-            babyage1_mon = Long.toString( (calDateDays/30)%12);
-            babyage2_mon = Long.toString( calDateDays/30);
-            babyage2_date = Long.toString( calDateDays%30);
-            babyage3_value = Long.toString( calDateDays/7);
-            babyage3_value_remain = Long.toString( calDateDays%7);
-
-            str = Long.toString(calDateDays);
-            str = str + "일";
-        }
-        catch(ParseException e)
-        {
-            // 예외 처리
-        }
-
-        //Textview 부분 색깔 강조
-        SpannableStringBuilder ssb = new SpannableStringBuilder(str);
-        ssb.setSpan(new ForegroundColorSpan(Color.parseColor("#01AFF1")), 0, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-
-
-
-        babyage1.setText("만 " + babyage1_year+"세 "+ babyage1_mon+"개월" );
-        babyage2.setText(babyage2_mon+"개월 "+babyage2_date+"일");
-        babyage3.setText(babyage3_value + "주 " + babyage3_value_remain + "일");
-        babyage4.setText(ssb);
-        current_data.setText(getTime);
-        baby_birth.setText(date2);
 
 
     return view;
@@ -242,5 +211,129 @@ public class Frag1 extends Fragment {
 
         }
     }
+
+
+
+
+
+
+
+
+
+    public class JsonParse extends AsyncTask<String, Void, String> {   // DB날짜 불러오는  클래스
+        String TAG = "JsonParseTest";
+        @Override
+        protected String doInBackground(String... strings) {
+            // execute의 매개변수를 받아와서 사용
+            String url = strings[0];
+            try {
+                URL serverURL = new URL(url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) serverURL.openConnection();
+
+                httpURLConnection.setReadTimeout(15000);
+                httpURLConnection.setConnectTimeout(15000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                } // 연결 상태 확인
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+                Log.d(TAG, sb.toString().trim());
+
+                return sb.toString().trim();        // 받아온 JSON 의 공백을 제거
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                return null;
+            }
+        }
+
+
+        protected void onPostExecute(String value){
+            super.onPostExecute(value);
+            String[] temp = value.split(" ");
+            date2 = temp[0]; // 날짜(생일)
+            String name = temp[1]; //이름
+            System.out.println(date2);
+
+
+            //현재 시간 구하기
+            long now = System.currentTimeMillis();
+            Date date = new Date(now);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String getTime = sdf.format(date);
+
+
+            //태어난지 얼마나 되었는지 날짜 차이 구하기
+            date1 = getTime;
+            System.out.println(date2+"asdsadasd");
+            try{
+                // date1, date2 두 날짜를 parse()를 통해 Date형으로 변환.
+                FirstDate = sdf.parse(date1);
+                SecondDate = sdf.parse(date2);
+
+                // Date로 변환된 두 날짜를 계산한 뒤 그 리턴값으로 long type 변수를 초기화 하고 있다.
+                // 연산결과 -950400000. long type 으로 return 된다.
+                long calDate = FirstDate.getTime() - SecondDate.getTime();
+
+                // Date.getTime() 은 해당날짜를 기준으로1970년 00:00:00 부터 몇 초가 흘렀는지를 반환해준다.
+                // 이제 24*60*60*1000(각 시간값에 따른 차이점) 을 나눠주면 일수가 나온다.
+                long calDateDays = calDate / ( 24*60*60*1000);
+                calDateDays = Math.abs(calDateDays);
+
+                //날짜 데이터 처리 부분
+                babyage1_year = Long.toString( calDateDays/30/12);
+                babyage1_mon = Long.toString( (calDateDays/30)%12);
+                babyage2_mon = Long.toString( calDateDays/30);
+                babyage2_date = Long.toString( calDateDays%30);
+                babyage3_value = Long.toString( calDateDays/7);
+                babyage3_value_remain = Long.toString( calDateDays%7);
+
+                str = Long.toString(calDateDays);
+                str = str + "일";
+            }
+            catch(ParseException e)
+            {
+                // 예외 처리
+            }
+
+            //Textview 부분 색깔 강조
+            SpannableStringBuilder ssb = new SpannableStringBuilder(str);
+            ssb.setSpan(new ForegroundColorSpan(Color.parseColor("#01AFF1")), 0, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+
+
+            babyage1.setText("만 " + babyage1_year+"세 "+ babyage1_mon+"개월" );
+            babyage2.setText(babyage2_mon+"개월 "+babyage2_date+"일");
+            babyage3.setText(babyage3_value + "주 " + babyage3_value_remain + "일");
+            babyage4.setText(ssb);
+            NameText.setText(name);
+            current_data.setText(getTime);
+            baby_birth.setText(date2);
+        }
+
+
+    }
+
+
+
 
 }
